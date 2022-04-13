a3895dc824b463e03941c99fc6de30f6c073fa95
Jorge-QA <jorge.q.a@hotmail.com>
2022-04-12 23:12:56 -0600

    Listo Editar

--- JS/logic-editRide.js
+++ JS/logic-editRide.js
@@ -111,3 +111,87 @@ function limpiarCheckBox() {
     document.getElementById("Friday").checked = false;
     document.getElementById("Saturday").checked = false;
 }
+
+function Dias() {
+    dias = [];
+    Sunday = document.getElementById("Sunday").checked;
+    if (Sunday) {
+        dias.push("Sunday");
+    }
+    Monday = document.getElementById("Monday").checked;
+    if (Monday) {
+        dias.push("Monday");
+    }
+    Tuesday = document.getElementById("Tuesday").checked;
+    if (Tuesday) {
+        dias.push("Tuesday");
+    }
+    Wednesday = document.getElementById("Wednesday").checked;
+    if (Wednesday) {
+        dias.push("Wednesday");
+    }
+    Thursday = document.getElementById("Thursday").checked;
+    if (Thursday) {
+        dias.push("Thursday");
+    }
+    Friday = document.getElementById("Friday").checked;
+    if (Friday) {
+        dias.push("Friday");
+    }
+    Saturday = document.getElementById("Saturday").checked;
+    if (Saturday) {
+        dias.push("Saturday");
+    }
+    return dias;
+}
+
+function registrarRide() {
+
+    let dias = Dias();
+
+    let comboRide = document.getElementById("RN");
+
+    if (comboRide.length != 0) {
+        let ridename = comboRide.options[comboRide.selectedIndex].text;
+        let from = document.getElementById("from").value;
+        let to = document.getElementById("to").value;
+        let description = document.getElementById("Dp").value;
+        let departure = document.getElementById("Dep").value;
+        let arrival = document.getElementById("EA").value;
+
+        let usuario = localStorage.getItem("UsuarioCargado");
+
+
+        if (ridename != "" & from != "" & to != "" & description != "" & departure != "" & arrival != "" & dias != "") {
+            let ride = { "usuario": usuario, "ridename": ridename, "from": from, "to": to, "description": description, "departure": departure, "arrival": arrival, "days": dias };
+            deleteRide(ridename);
+            guardarRide(ride);
+            alert(ridename + " se ha modificado con éxito...");
+        } else {
+            alert("Ingrese todos los datos solicitados por favor...");
+        }
+    }
+}
+
+// guarda objetos ride persona en local storage en un array
+function guardarRide(ride) {
+    let RidesArray = JSON.parse(localStorage.getItem("DataRides")) || [];
+    RidesArray.push(ride);
+    let RidesArrayJ = JSON.stringify(RidesArray);
+    localStorage.setItem("DataRides", RidesArrayJ);
+}
+
+function deleteRide(rideName) {
+    let listaRides = recuperarDataRides();
+
+    //Devuelve -1 si no hay coincidencia.... ojo!
+    rideIndexInArray = listaRides.findIndex(element => element.ridename === rideName);
+
+    listaRides.splice(rideIndexInArray, 1);
+
+    let RidesArrayJ = JSON.stringify(listaRides);
+    localStorage.setItem("DataRides", RidesArrayJ);
+
+    //Alternativa creando Array filtradoi (poco ágil...)
+    //let newrides = listaRides.filter(element => element.ridename !== rideName)
+}
